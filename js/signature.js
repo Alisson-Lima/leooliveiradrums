1e0d9a9c3bc67b87aeec0f4cdeeaf41b29163ab8
Alisson Lima <renalima.rl46@gmail.com>
2023-09-30 15:37:04 -0300

    Adding animation in signature 2.0

---
+++ js/signature.js
@@ -0,0 +1,16 @@
+(function(){
+
+    const svgSign = document.querySelector("#signature")
+
+    const observer = new IntersectionObserver(entries =>{
+        console.log(entries)
+        if(entries[0].isIntersecting){
+            entries[0].target.classList.add("active")
+        }
+    },{
+        threshold: .5
+    })
+
+    observer.observe(svgSign)
+
+})()
