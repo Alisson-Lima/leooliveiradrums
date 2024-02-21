fe676d07e5c71b1ae00ff19b649dfca49f46bfc9
Alisson Lima <renalima.rl46@gmail.com>
2024-02-21 14:38:10 -0300

    fix: add oficial video and thumbnail

--- js/video.js
+++ js/video.js
@@ -2,6 +2,6 @@ const thumb = document.querySelector(".video");
 
 thumb.addEventListener("click", ()=>{
     thumb.innerHTML = `
-        <iframe width="100%" height="100%" style="border-radius: 16px;" src="https://www.youtube.com/embed/WthTWqZc8Jk" title="ME ATRAIU | Gabriela Rocha | DRUM COVER" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <iframe width="100%" height="100%" style="border-radius: 16px" src="https://www.youtube.com/embed/JCD-b2SVQNg?si=_b9-PNPjtqxj3KS3&amp;start=1" title="CURSO DE RUDIMENTOS NA PRÁTICA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     `;
 });
